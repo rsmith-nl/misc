2421db755870a8cd9743de85465edda71dcf7d1d
Roland Smith <rsmith@xs4all.nl>
2015-09-05 17:24:49 +0200

    Added timezone argument to fcdate.

--- filedate.py
+++ filedate.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
 # file: filedate.py
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2014-12-17 23:30:49 +0100
-# Last modified: 2015-09-05 14:23:14 +0200
+# Last modified: 2015-09-05 15:11:55 +0200
 #
 # To the extent possible under law, R.F. Smith has waived all copyright and
 # related or neighboring rights to filedate.py. This work is published
@@ -17,20 +16,26 @@ from pytz import timezone
 _here = timezone('Europe/Amsterdam')
 
 
-def fcdate(name):
+def fcdate(name, tz=_here):
     """Get the creation date of a file formatted as a string.
 
-    :param name: name of the file to query
-    :returns: @todo
+    Arguments
+        name: Name of the file to query.
+        tz: Local timezone. Defaults to Europe/Amsterdam
+
+    Returns:
+        String containing the formatted date or None.
     """
+    if isinstance(tz, str):
+        tz = timezone(tz)
     try:
-        bt = datetime.fromtimestamp(stat(name).st_birthtime, _here)
+        bt = datetime.fromtimestamp(stat(name).st_birthtime, tz)
+        rv = bt.strftime('%F %T %z')
     except OSError:
-        bt = datetime.now(_here)
-    rv = bt.strftime('%F %T %z')
+        rv = None
     return rv
 
 # Tests
 if __name__ == '__main__':
-    for fn in ['functest.c', 'nonexistent']:
+    for fn in ['nameddict.py', 'nonexistent']:
         print(fn, fcdate(fn))
