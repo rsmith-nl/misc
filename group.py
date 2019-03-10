ee6978850d9bb26a04b911bc04b61186ec8bbd51
Roland Smith <rsmith@xs4all.nl>
2019-03-10 21:11:07 +0100

    Replace groupby with chunks.

--- group.py
+++ group.py
@@ -4,27 +4,22 @@
 # Copyright © 2014 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2014-08-01T13:31:09+0200
-# Last modified: 2018-04-17T20:48:39+0200
+# Last modified: 2019-03-10T21:10:28+0100
 """Group an interator into sub-iterators."""
 
-__version__ = '0.1.1'
+__version__ = '0.2'
 
 
-def subgroup(it, n):  # {{{1
-    """Split an iterator into a list of sub-iterators of size n.
-
-    Arguments:
-        it: Iterator to divide
-        n: Length of sub-iterators
-
-    Returns:
-        A list of sub-iterators.
-    """
-    return [it[i:i + n] for i in range(0, len(it), n)]
+def chunks(sequence, count):  # {{{1
+    """Split an sequence into chunks of count items."""
+    if count < 1:
+        raise ValueError('count must be > 0')
+    for i in range(0, len(sequence), count):
+        yield sequence[i:i+count]
 
 
 if __name__ == '__main__':  # Test {{{1
     f = range(10)
     print('Test for group.py')
     print('f:', f)
-    print('subgroup(f, 3):', subgroup(f, 3))
+    print('chunks(f, 3):', chunks(f, 3))
