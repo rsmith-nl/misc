25acf511910e4fa912dd8d8f4a0d36f6360c4cf1
Roland Smith <rsmith@xs4all.nl>
2019-05-22 23:52:13 +0200

    Add dround.

---
+++ dround.py
@@ -0,0 +1,23 @@
+# file: dround.py
+# vim:fileencoding=utf-8:fdm=marker:ft=python
+#
+# Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>
+# Created: 2019-05-22T22:55:03+0200
+# Last modified: 2019-05-22T23:51:47+0200
+"""Use the decimal module to round floating point numbers in a reasonable way.
+
+>>> dround(0.999999)
+1.0
+>>> dround(0.006599)
+0.0066
+>>> dround(0.0749999)
+0.075
+"""
+
+from decimal import Decimal
+
+
+def dround(num):
+    dec = Decimal(num)
+    adj = abs(dec.adjusted())+1
+    return round(num, adj)
