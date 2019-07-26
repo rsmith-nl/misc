be488d12858f92f5ae313f8ef2f64bec9352ddaf
Roland Smith <rsmith@xs4all.nl>
2019-07-26 23:38:00 +0200

    Use chunked idea from more_itertools.

--- group.py
+++ group.py
@@ -4,22 +4,16 @@
 # Copyright © 2014 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2014-08-01T13:31:09+0200
-# Last modified: 2019-03-10T21:10:28+0100
-"""Group an interator into sub-iterators."""
+# Last modified: 2019-07-26T23:36:56+0200
+"""Group an iterator into sub-iterators."""
 
-__version__ = '0.2'
+__version__ = '0.3'
 
-
-def chunks(sequence, count):  # {{{1
-    """Split an sequence into chunks of count items."""
-    if count < 1:
-        raise ValueError('count must be > 0')
-    for i in range(0, len(sequence), count):
-        yield sequence[i:i+count]
+import itertools as it
+import functools as ft
 
 
-if __name__ == '__main__':  # Test {{{1
-    f = range(10)
-    print('Test for group.py')
-    print('f:', f)
-    print('chunks(f, 3):', chunks(f, 3))
+def chunked(iterable, n):  # {{{1
+    def take(n, iterable):
+        return list(it.islice(iterable, n))
+    return iter(ft.partial(take, n, iter(iterable)), [])
