4ca15834c7fff9d9320e0e31a2647fda0dd1cd58
Roland Smith <rsmith@xs4all.nl>
2019-08-08 23:06:13 +0200

    Explain chunked with a simpler version.

--- group.py
+++ group.py
@@ -4,7 +4,7 @@
 # Copyright © 2014 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2014-08-01T13:31:09+0200
-# Last modified: 2019-07-26T23:36:56+0200
+# Last modified: 2019-08-08T23:05:41+0200
 """Group an iterator into sub-iterators."""
 
 __version__ = '0.3'
@@ -14,6 +14,29 @@ import functools as ft
 
 
 def chunked(iterable, n):  # {{{1
+    """
+    Split an iterable up in chunks of length n.
+
+    The second argument to the outer ``iter()`` is crucial to the way this works.
+    See the documentation for ``iter()`` for details.
+    """
     def take(n, iterable):
         return list(it.islice(iterable, n))
     return iter(ft.partial(take, n, iter(iterable)), [])
+
+
+# def chunked2(iterable, n):
+#     """
+#     Split an iterable up in chunks of length n.
+#     (simple version)
+#     """
+#     itr = iter(iterable)
+#     res = []
+#     try:
+#         while True:
+#             for j in range(n):
+#                 res.append(next(itr))
+#             yield res
+#             res = []
+#     except StopIteration:
+#         yield res
