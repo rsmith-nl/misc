14d7d540fd499c73d3f860f9a55363f27ba904c2
Roland Smith <rsmith@xs4all.nl>
2020-05-14 19:18:16 +0200

    Add controls to ANSI module.

--- ansi.py
+++ ansi.py
@@ -4,9 +4,19 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-07-13T12:43:16+0200
-# Last modified: 2019-07-13T12:51:59+0200
+# Last modified: 2020-05-14T19:14:06+0200
+"""
+Predefined ANSI escape codes.
+
+https://en.wikipedia.org/wiki/ANSI_escape_code
+
+Example: print red text
+>>> import ansi
+>>> print(ansi.fgcolor.brightred+"Danger, Will Robinson!"+ansi.fgcolor.reset)
+"""
 
 from types import SimpleNamespace
+
 
 # Standard ANSI colors.
 fgcolor = SimpleNamespace(
@@ -43,4 +53,16 @@ bgcolor = SimpleNamespace(
     cyan='\033[46m',
     white='\033[47m',
     reset='\033[0m'
+)
+
+control = SimpleNamespace(
+    CHA='\033[1G',  # move cursor to column 1
+    EL='\033[0K',   # clear to end of line
+    EL2='\033[2K',  # clear whole line
+    ED='\033[0J',   # clear to end of screen
+    ED2='\033[2J',  # clear whole screen
+    CUU='\033[1A',  # move cursor up one line
+    CUD='\033[1B',  # move cursor down one line
+    CUF='\033[1C',  # move cursor forward one column
+    CUB='\033[1D',  # move cursor backward one column
 )
