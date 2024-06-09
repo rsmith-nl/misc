acffafeee3d5d69d0963f945e7eb36576e8fd8c8
Roland Smith <rsmith@xs4all.nl>
2024-06-09 17:31:38 +0200

    Add test to group.py.

--- group.py
+++ group.py
@@ -4,7 +4,7 @@
 # Copyright © 2014 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2014-08-01T13:31:09+0200
-# Last modified: 2022-01-25T00:31:49+0100
+# Last modified: 2024-06-09T17:31:05+0200
 """Group an iterator into sub-iterators."""
 
 __version__ = "2019.08.08"
@@ -19,30 +19,13 @@ def chunked(iterable, n):  # {{{1
 
     The second argument to the outer ``iter()`` is crucial to the way this works.
     See the documentation for ``iter()`` for details.
-    """
 
+    Example:
+    >>> from group import chunked
+    >>> list(chunked(range(10), 4))
+    [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9]]
+    """
     def take(n, iterable):
         return list(it.islice(iterable, n))
 
     return iter(ft.partial(take, n, iter(iterable)), [])
-
-
-def chunked2(iterable, n):
-    return iter(ft.partial(lambda m, j: list(it.islice(j, m)), n, iter(iterable)), [])
-
-
-# def chunked2(iterable, n):
-#     """
-#     Split an iterable up in chunks of length n.
-#     (simple version)
-#     """
-#     itr = iter(iterable)
-#     res = []
-#     try:
-#         while True:
-#             for j in range(n):
-#                 res.append(next(itr))
-#             yield res
-#             res = []
-#     except StopIteration:
-#         yield res
